3429c3555b8714e59456d17f8b37de7de997edfe
Therese Goshen <thgo8749@colorado.edu>
2024-11-06 15:26:51 -0700

    created create route
    created a route that creates a table called Basketball.

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_test():
     conn = psycopg2.connect("postgresql://goshen_db_user:lN6qAMBw37XksBf4CrF9NPlSHpw5loXf@dpg-csluhru8ii6s73b7agc0-a/goshen_db")
     conn.close()
     return 'Database connection successful'
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgresql://goshen_db_user:lN6qAMBw37XksBf4CrF9NPlSHpw5loXf@dpg-csluhru8ii6s73b7agc0-a/goshen_db")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return 'Table created'
